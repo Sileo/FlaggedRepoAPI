bfacd7f8363be088d0ac16840a7b0546d5ea0238
Oliver Boudet <oboudet1@me.com>
2018-12-30 11:03:34 +0100

    Revert back to the old regex way.

--- controllers/flagged.js
+++ controllers/flagged.js
@@ -1,10 +1,56 @@
+const { parse } = require("url");
 const { ApiRequest } = require("../database/entities/ApiRequest");
 const repolist = require("../config/flagged-repos.json");
 const flaggedRepos = repolist.flaggedRepoURLs;
 
-exports.flaggedController = (req, res) => {
+let repoRegexes = [];
+
+const createRegex = (url) => {
+    // basically https://stackoverflow.com/a/39095107/9784092 but better
+    const split = /([^\/]+)(\/.*)?/;
+
+    const matches = url.match(split);
+    if (!matches) return null;
+
+    var preparedURL = "";
+    preparedURL = `${matches[1]}`
+        .replace(/[?()[\]\\.+^$|]/g, "\\$&")
+        .replace(/\*\\./g, "(?:[^/]*\\.)*")
+        .replace(/\*$/, "[^/]*");
+
+    if (matches[2]) {
+        preparedURL += matches[2]
+            .replace(/[?()[\]\\.+^$|]/g, "\\$&")
+            .replace(/\/\*(?=$|\/)/g, "(?:/[^]*)?");
+    }
+    return new RegExp(`^${preparedURL}$`, "i");
+};
+
+const regenerateRegex = () => {
+    console.log("Generating regex array.");
+    for (const repo of flaggedRepos) {
+        const regex = createRegex(repo);
+        if (!regex)
+            return console.error(`Failed to generate regex for ${repo}.`);
+
+        repoRegexes.push(regex);
+    }
+    console.log("Generated regex array.");
+};
+
+const flaggedController = (req, res) => {
     if (!req.body.url) return res.status(400).end();
-    const isPiracy = flaggedRepos.some((x) => req.body.url.includes(x));
+    const parsed = parse(req.body.url);
+    // prettier-ignore
+    const normalised = `${parsed.hostname}${parsed.pathname == "/" ? "" : parsed.pathname}`
+        .replace(/^www\./, "")
+        .trim();
+
+    console.log(normalised);
+
+    const isPiracy = repoRegexes.some((regex) => {
+        return normalised.match(regex) !== null;
+    });
     ApiRequest.sync().then(() => {
         ApiRequest.create({
             isPiracy
@@ -12,3 +58,5 @@ exports.flaggedController = (req, res) => {
     });
     return res.send(isPiracy);
 };
+
+module.exports = { regenerateRegex, flaggedController };
--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const { parse } = require("url");
 const express = require("express");
 const { initDatabase } = require("./database/index");
-const { ApiRequest } = require("./database/entities/ApiRequest");
 const { infoController } = require("./controllers/info");
-const { flaggedController } = require("./controllers/flagged");
+const { flaggedController, regenerateRegex } = require("./controllers/flagged");
 const { homepageController } = require("./controllers/homepageController");
 const app = express();
-
-const repolist = require("./config/flagged-repos.json");
-const flaggedRepos = repolist.flaggedRepoURLs;
 
 app.use(express.json());
 
@@ -18,6 +14,7 @@ app.post("/flagged", flaggedController);
 
 initDatabase().then(() => console.log("Database connected."));
 
+regenerateRegex();
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
     console.log(`Flagged Repo API listening on http://localhost:${port}.`)
